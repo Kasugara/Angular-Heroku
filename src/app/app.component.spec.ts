037b2943337356e262f4ce2e4c2e6dbfcc8a2ed0
Kasugara <Ertanto_andre11@yahoo.com>
2022-04-26 10:17:51 +0700

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'WholeNewWorldLoginEmployee'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WholeNewWorldLoginEmployee');
+    expect(app['title']).toEqual('WholeNewWorldLoginEmployee');
   });
 
   it('should render title', () => {
