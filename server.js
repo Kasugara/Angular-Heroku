d2e78a26f7cdc6a585306a7d81bc63fa102d8dfc
Kasugara <Ertanto_andre11@yahoo.com>
2022-04-28 15:06:30 +0700

    kekw

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory 
-app.use(express.static('./dist/whole-new-world-loginn-employee'));
+app.use(express.static('./dist/whole-new-world-login-employee'));
 
 app.get('/*', (req,res) => 
-    res.sendFile('index.html', {root: 'dist/whole-new-world-loginn-employee/'}),
+    res.sendFile('index.html', {root: 'dist/whole-new-world-login-employee/'}),
 );
 
 
