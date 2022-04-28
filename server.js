af42e417c721cdbf19cbe79b5615f07d3f1bf82c
Kasugara <Ertanto_andre11@yahoo.com>
2022-04-28 11:03:16 +0700

    kek

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory 
-app.use(express.static('./dist/WholeNewWorldLoginEmployee'));
+app.use(express.static('./dist/whole-new-world-loginn-employee'));
 
 app.get('/*', (req,res) => 
-    res.sendFile('index.html', {root: 'dist/WholeNewWorldLoginEmployee/'}),
+    res.sendFile('index.html', {root: 'dist/whole-new-world-loginn-employee/'}),
 );
 
-//start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
 
