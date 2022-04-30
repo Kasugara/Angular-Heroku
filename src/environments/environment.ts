7d38e6ba6a19dd11061e4ff6c941d220e8de208a
Kasugara <Ertanto_andre11@yahoo.com>
2022-04-30 14:00:24 +0700

    kekw

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080' 
+  apiBaseUrl: 'https://mr-realty-dbe-be.herokuapp.com',
+  apiBaseUrl1: 'https://localhost:8080' 
 };
 
 /*
