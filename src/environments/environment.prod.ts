83ab2ad50802a11bca295598a078566ee03d72dc
Kasugara <Ertanto_andre11@yahoo.com>
2022-06-24 16:10:07 +0700

    bismillah

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://mrrealtydbe.herokuapp.com'
+  apiBaseUrl: 'https://mrrealtydbe.herokuapp.com',
+  apiBaseUrl1: 'https://localhost:8080'
 };
